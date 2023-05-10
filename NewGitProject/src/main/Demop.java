3f4bd60d24724e5160def26f2b98c744338195bc
Aniket <anikethumane777@gmail.com>
2023-05-10 11:49:13 +0530

    change success

---
+++ NewGitProject/src/main/Demop.java
@@ -0,0 +1,13 @@
+package src.main;
+
+public class Demop {
+
+	Demop(){
+		System.out.println("hhask");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
